a2852ae94af79b4d0eb330d752566b7ad4a6f612
mynameismaxz <sirachat.sir@gmail.com>
2020-02-04 22:34:04 +0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mynameismaxz/websocket_client_golang
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.1
+	github.com/joho/godotenv v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+
+	"github.com/gorilla/websocket"
+)
+
+// var addr = flag.String("addr", "api.debug.rtt.in.th:11055", "http service address")
+
+func main() {
+	// load environment data
+	loadEnvironment()
+
+	// get parameter name
+	wsUrl := os.Getenv("HOSTNAME")
+	protocolWs := os.Getenv("PROTOCOL")
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: protocolWs, Host: wsUrl}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+
+	go func() {
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	for {
+		select {
+		case <-interrupt:
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Fatal("Error to close message")
+				return
+			}
+			defer c.Close()
+			return
+		}
+	}
+}
+
+func loadEnvironment() {
+	log.Println("Checking env...")
+	requiredVariable := []struct {
+		name         string
+		defaultValue string
+	}{
+		{"HOSTNAME", ""},
+		{"PROTOCOL", ""},
+	}
+	for _, env := range requiredVariable {
+		value, set := os.LookupEnv(env.name)
+		if !set {
+			log.Fatal("Require parameter: \"%s\"", env.name)
+		}
+		log.Printf("%s: %s", env.name, value)
+	}
+}
